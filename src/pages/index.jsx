be38dffdf7ef34d0891116becc7abe5327252802
fatmaist <fatmaistyn@gmail.com>
2023-11-18 22:46:37 +0700

    feat(frontend) : Add file frontend and create modules

---
+++ src/pages/index.jsx
@@ -0,0 +1,39 @@
+import Wrapper from "@/components/Wrapper";
+import { prisma } from "@/utils/prisma";
+import Books from "../components/Books";
+import Cookies from "js-cookie";
+
+export default function Homepage(props) {
+  return (
+    <Wrapper>
+      {props?.books?.map((book) => (
+        <Books key={`${book.id} ${book.title}`} {...book} />
+      ))}
+    </Wrapper>
+  );
+}
+
+// server side props ( fetch in server )
+
+export async function getServerSideProps() {
+  try {
+    const books = await prisma.book.findMany({
+      orderBy: {
+        title: "asc",
+      },
+    });
+    return {
+      props: {
+        books,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    // Jika terjadi kesalahan, kembalikan pesan kesalahan sebagai props
+    return {
+      props: {
+        error: { message: "Something went wrong" }
+      }
+    };
+  }
+}
